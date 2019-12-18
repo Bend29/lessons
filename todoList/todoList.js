728e0d727cf4f30d8efc3ae9b390dd7067c8418d
kruglova <kruglova@internet-design.ru>
2019-12-18 13:18:03 +0300

    Добавлены фильтры и кнопка Завершена

--- todoList/todoList.js
+++ todoList/todoList.js
@@ -1,9 +1,27 @@
-const tasks = [];
+const tasks = [
+    {
+        _id: '5d2ca9e2e03d40b326596aa7',
+        completed: true,
+        body:
+            'Occaecat non ea quis occaecat ad culpa amet deserunt incididunt elit fugiat pariatur. Exercitation commodo culpa in veniam proident laboris in. Excepteur cupidatat eiusmod dolor consectetur exercitation nulla aliqua veniam fugiat irure mollit. Eu dolor dolor excepteur pariatur aute do do ut pariatur consequat reprehenderit deserunt.\r\n',
+        title: 'Eu ea incididunt sunt consectetur fugiat non.',
+    },
+    {
+        _id: '5d2ca9e29c8a94095c1288e0',
+        completed: false,
+        body:
+            'Aliquip cupidatat ex adipisicing veniam do tempor. Lorem nulla adipisicing et esse cupidatat qui deserunt in fugiat duis est qui. Est adipisicing ipsum qui cupidatat exercitation. Cupidatat aliqua deserunt id deserunt excepteur nostrud culpa eu voluptate excepteur. Cillum officia proident anim aliquip. Dolore veniam qui reprehenderit voluptate non id anim.\r\n',
+        title:
+            'Deserunt laborum id consectetur pariatur veniam occaecat occaecat tempor voluptate pariatur nulla reprehenderit ipsum.',
+    },
+];
 
 (function (arrOfTasks) {
+
     const objOfTask = arrOfTasks.reduce((acc, task) => {
         acc[task._id] = task;
         return acc;
+
     }, {});
 
     // цвета для тем
@@ -76,20 +94,24 @@ const tasks = [];
 
     let lastSelectedTheme = localStorage.getItem('app_theme') || 'default';
     setTheme(lastSelectedTheme);
+
     //elements UI
     const listContainer = document.querySelector('.tasks-list-section .list-group');
-
     const form = document.forms['addTask'];
     const inputTitle = form.elements['title'];
     const inputBody = form.elements['body'];
     const themeSelect = document.getElementById('themeSelect');
+    const filters = document.querySelector('.js-filters-nav');
+    const filterBtn = filters.querySelectorAll('.js-filter-btn');
 
     renderAllTasks(objOfTask);
 
     //events
     form.addEventListener('submit', onFormSubmitHandler);
     listContainer.addEventListener('click', onDeleteHandler);
+    listContainer.addEventListener('click', onCompletedHandler);
     themeSelect.addEventListener('change', onThemeSelectHandler);
+    filters.addEventListener('click', filterTasksHandler);
 
     function renderAllTasks(tasksList) {
         if (!tasksList) {
@@ -102,34 +124,57 @@ const tasks = [];
         Object.values(tasksList).forEach(task => {
             const li = listItemTemplate(task);
             fragment.appendChild(li);
+            if (task.completed === true) {
+                li.classList.add('completed');
+                checkTaskOfCompleted(li.dataset.taskId, li);
+            }
         });
 
         listContainer.appendChild(fragment);
-
+        filterTasks();
+        setMessage();
     }
 
-    function listItemTemplate({_id, title, body} = {}) {
+    function listItemTemplate({_id, title, body, completed} = {}) {
         const li = document.createElement('li');
         li.classList.add(
+            'item-list',
+            'js-item-list',
             'list-group-item',
-            'd-flex',
             'align-items-center',
             'flex-wrap',
             'mt-2'
         );
 
         li.setAttribute('data-task-id', _id);
+        if (completed === true) {
+            setAttributeCompleted(li, 'is-completed');
+        } else {
+            setAttributeCompleted(li, 'not-completed');
+        }
+
+
         const span = document.createElement('span');
         span.textContent = title;
         span.style.fontWeight = 'bold';
+        span.style.width = '75%';
 
         const button = document.createElement('button');
         button.textContent = 'Delete task';
         button.classList.add(
             'btn',
             'btn-danger',
+            'js-delete-btn'
+        );
+
+        const checkButton = document.createElement('button');
+        checkButton.textContent = 'Сompleted';
+        checkButton.classList.add(
+            'btn',
             'ml-auto',
-            'delete-btn'
+            'btn-success',
+            'mr-2',
+            'js-completed-btn'
         );
 
         const article = document.createElement('p');
@@ -138,9 +183,12 @@ const tasks = [];
             'mt-2',
             'w-100'
         );
+
         li.appendChild(span);
+        li.appendChild(checkButton);
         li.appendChild(button);
         li.appendChild(article);
+        setMessage();
 
         return li;
     }
@@ -157,7 +205,10 @@ const tasks = [];
         const task = createNewTask(titleValue, bodyValue);
         const listItem = listItemTemplate(task);
         listContainer.insertAdjacentElement('afterbegin', listItem);
+        setMessage();
         form.reset();
+        filterTasks();
+
     }
 
     function createNewTask(title, body) {
@@ -168,6 +219,7 @@ const tasks = [];
             _id: `task-${Math.random()}`
         };
         objOfTask[newTask._id] = newTask;
+
         return {...newTask};
     }
 
@@ -176,16 +228,18 @@ const tasks = [];
         const isConfirm = confirm(`Удалить задачу ${title}?`);
         if (!isConfirm) return isConfirm;
         delete objOfTask[id];
+
         return isConfirm;
     }
 
     function deleteTaskFromHtml(confirmed, el) {
         if (!confirmed) return;
         el.remove();
+        setMessage();
     }
 
     function onDeleteHandler({target}) {
-        if (target.classList.contains('delete-btn')) {
+        if (target.classList.contains('js-delete-btn')) {
             const parent = target.closest('[data-task-id]');
             const id = parent.dataset.taskId;
             const confirmed = deleteTask(id);
@@ -193,7 +247,50 @@ const tasks = [];
         }
     }
 
-    function onThemeSelectHandler(e) {
+    /**
+     * меняем значение ключа completed
+     * @param id - id задачи
+     * @param value - значение true/false
+     */
+    function changeCompletion(id, value) {
+        objOfTask[id].completed = value;
+
+    }
+
+    /**
+     * в зависимости от наличия класса меняем значение ключа completed
+     * @param id - id задачи
+     */
+
+    function checkTaskOfCompleted(id, element) {
+        element.classList.contains('completed') ? changeCompletion(id, true) : changeCompletion(id, false);
+    }
+
+    function setAttributeCompleted(elem, value) {
+        elem.setAttribute('data-task-completed', value);
+        filterTasks();
+    }
+
+    /**
+     * обработчик клика по кнопке отметки о выполнении
+     * @param target - параметр event
+     */
+    function onCompletedHandler({target}) {
+        if (target.classList.contains('js-completed-btn')) {
+            const parent = target.closest('[data-task-id]');
+            const id = parent.dataset.taskId;
+            parent.classList.toggle('completed');
+            checkTaskOfCompleted(id, parent);
+            if (parent.classList.contains('completed')) {
+                setAttributeCompleted(parent, 'is-completed');
+            } else {
+                setAttributeCompleted(parent, 'not-completed');
+            }
+
+        }
+    }
+
+    function onThemeSelectHandler() {
         const selectedTheme = themeSelect.value;
         const isConfirmed = confirm(`Вы действительно хотите изменить тему на ${selectedTheme}?`);
         if (!isConfirmed) {
@@ -205,11 +302,49 @@ const tasks = [];
         localStorage.setItem('app_theme', selectedTheme);
     }
 
+
+    function filterTasksHandler({target}) {
+        for (let i = 0; i < filterBtn.length; i++) {
+            filterBtn[i].classList.remove('active');
+        }
+        target.classList.add('active');
+        filterTasks();
+    }
+
+    function filterTasks() {
+        const listItem = document.querySelectorAll('.js-item-list');
+        const dataValue = filters.querySelector('.js-filter-btn.active').getAttribute('data-filter');
+        for (let i = 0; i < listItem.length; i++) {
+            if (listItem[i].getAttribute('data-task-completed') !== dataValue) {
+                listItem[i].style.display = 'none';
+            } else {
+                listItem[i].style.display = 'flex';
+            }
+            if(dataValue === 'all') {
+                listItem[i].style.display = 'flex';
+            }
+        }
+    }
+
     function setTheme(name) {
         const selectedThemeObj = themes[name];
-       Object.entries(selectedThemeObj).forEach(([key, value]) => {
-          document.documentElement.style.setProperty(key, value);
-       });
+        Object.entries(selectedThemeObj).forEach(([key, value]) => {
+            document.documentElement.style.setProperty(key, value);
+        });
     }
+
+    function setMessage() {
+        const textMessage = document.querySelector('.js-text-message');
+        const listItem = document.querySelectorAll('.js-item-list');
+        if (!listItem.length) {
+            textMessage.textContent = 'Создайте задачу';
+            filters.style.display = 'none'
+        } else {
+            textMessage.textContent = '';
+            filters.style.display = 'block';
+        }
+        return textMessage;
+    }
+
 
 })(tasks);
